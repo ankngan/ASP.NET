94becb110345592737bbe1a697dbad384bbf3533
trungthuc3097 <33407272+trungthuc3097@users.noreply.github.com>
2017-12-04 15:19:37 +0700

    delete
    delete

--- trunk/procedure/proc.sql
+++
@@ -1,9 +0,0 @@
-create proc LoadProductDetail
-@id int
-as
-begin
-	select Product.*, Main_detail.*
-	from Product
-	inner join Main_detail
-	on Product.main_detail_id = Main_detail.main_detail_id and Product.product_id = @id
-end
